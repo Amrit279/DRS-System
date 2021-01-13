e84cae6e6e51bbe048fd9e2bc8f9b9f123c77b09
Amrit279 <amrit887tiwari@gmail.com>
2021-01-13 20:06:03 +0530

    Added Buttons in the gui window

--- main.py
+++ main.py
@@ -20,6 +20,19 @@ class DRS(tk.Tk):
         self.canvas.create_image(0, 0, image = self.photo, anchor = tk.NW)
         self.canvas.pack()
 
+        self.but = tk.Button(self, text = "<< Previous (Fast)", padx = 2.6)
+        self.but.pack()
+        self.but = tk.Button(self, text = "<< Previous (Slow)")
+        self.but.pack()
+        self.but = tk.Button(self, text = "Next (Fast) >>", padx = 13.4)
+        self.but.pack()
+        self.but = tk.Button(self, text = "Next (Slow) >>", padx = 11)
+        self.but.pack()
+        self.but = tk.Button(self, text = "Give Out", padx = 27.5)
+        self.but.pack()
+        self.but = tk.Button(self, text = "Give Not Out", padx = 16.4)
+        self.but.pack()
+
 if __name__ == "__main__":
     drs = DRS()
     drs.mainloop()
